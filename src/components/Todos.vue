c93c7fff1e0ba383445660952117767cf69da614
Karunajeewa <karunajeewapws@gmail.com>
2020-12-25 23:18:21 +0530

    version one is completed

---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItems v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItems from './TodosItems.vue';
+
+export default {
+    name:"Todos",
+    components:{
+        TodoItems
+    },
+    props:["todos"]
+}
+</script>
+
+<style scoped>
+</style>
